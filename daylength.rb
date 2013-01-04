ad0322974a26313f3c4c4a6140946d7ee929b505
Ricco Førgaard <ricco@fiskeben.dk>
2013-01-04 12:54:24 +0100

    Added map.

--- daylength.rb
+++ daylength.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 require 'date'
 require './lib/geolocator'
 require './lib/sincesolstice'
@@ -13,15 +14,38 @@ get '/' do
   locals = { :ok => true }
   
   if locator.success?
-    sincesolstice = SinceSolstice.new locator.latitude, today
-  
-    locals[:place] = locator.city
-    locals[:solstice] = sincesolstice.solstice.strftime "%d.%m %Y"
-    locals[:hours] = sincesolstice.hours
-    locals[:minutes] = sincesolstice.minutes
+    latitude = locator.latitude
+    longitude = locator.longitude
+    locals[:ok] = true
   else
+    latitude = 55.5
+    longitude = 9.5
     locals[:ok] = false
   end
   
+  sincesolstice = SinceSolstice.new latitude, today
+  
+  locals[:place] = (!locator.city.nil? and locator.city != "" ) ? locator.city : nil
+  locals[:latitude] = latitude
+  locals[:longitude] = longitude
+  locals[:solstice] = sincesolstice.solstice.strftime "%d.%m %Y"
+  locals[:hours] = sincesolstice.hours
+  locals[:minutes] = sincesolstice.minutes
+  
   erb :index, :locals => locals
+end
+
+get '/updatelocation' do
+  today = Date.today
+  sincesolstice = SinceSolstice.new params['lat'], today
+  
+  data = {
+    :place => nil,
+    :solstice => sincesolstice.solstice.strftime("%d.%m %Y"),
+    :hours => sincesolstice.hours,
+    :minutes => sincesolstice.minutes
+  }
+  
+  content_type :json
+  data.to_json
 end

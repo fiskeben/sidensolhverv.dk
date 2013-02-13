52a7c7616b9d2c9fdb95a1b0018071025ff9ac25
Ricco Førgaard <ricco@fiskeben.dk>
2013-02-13 22:43:51 +0100

    Changed location detection to javascript-based.

--- daylength.rb
+++ daylength.rb
@@ -3,7 +3,6 @@ require 'sinatra/cookies'
 require 'sinatra/multi_route'
 require 'json'
 require 'date'
-require 'geolocator'
 require 'sincesolstice'
 require 'month'
 
@@ -15,17 +14,9 @@ def get_location
     location[:ok] = true
     location[:place] = nil
   else
-    locator = Geolocator.new request.ip
-    if locator.success?
-      location[:latitude] = locator.latitude
-      location[:longitude] = locator.longitude
-      location[:place] = locator.city
-      location[:ok] = true
-    else
-      location[:latitude] = 55.5
-      location[:longitude] = 9.5
-      location[:ok] = false
-    end
+    location[:latitude] = 55.5
+    location[:longitude] = 9.5
+    location[:ok] = false
   end
   
   location

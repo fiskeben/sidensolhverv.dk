b171a205ac2f044f79f55ed56d7e70c7c4e4c5af
fiskeben <ricco@fiskeben.dk>
2016-11-26 18:59:13 +0100

    Added Google Maps API key

--- app/app.rb
+++ app/app.rb
@@ -6,14 +6,15 @@ require 'date'
 class App < Sinatra::Application
   set :root, File.join(File.dirname(__FILE__), '..')
   set :views, Proc.new { File.join(root, 'views') }
-  
+  set :google_maps_api_key, ENV['GOOGLE_MAPS_API_KEY']
+
   configure do
     set :public_folder, root + '/public'
   end
 
   get '/' do
     locals = {}
-    
+
     if (params['date'] && params['lat'] && params['lng'])
       begin
         date = Date.strptime params['date'], "%Y-%m-%d"
@@ -24,15 +25,15 @@ class App < Sinatra::Application
     else
       locals[:presets] = {}
     end
-      
-    erb :application, :locals => {:partial => :index, :partial_locals => locals}
+
+    erb :application, :locals => {:partial => :index, :partial_locals => locals, :google_maps_api_key => settings.google_maps_api_key}
   end
 
   get '/api/?' do
     erb :application, :locals => { :partial => :api, :partial_locals => {} }
   end
 
-  get '/om/?' do 
+  get '/om/?' do
     erb :application, :locals => { :partial => :about, :partial_locals => {} }
   end
 end
